6620f37c8022747acf69092849e299a2064401f5
babirye grace <babiegrace46@gmail.com>
2020-07-16 09:19:54 +0300

    test.js

--- test/coderefactor.js
+++ test/coderefactor.js
@@ -1,10 +1,17 @@
 const assert = require("chai").assert;
-const R = require("../computeGeometries");
-
+const R = require("./computeGeometries");
 
 describe("test for the correct area", function () {
   let computeGeometries = R;
   it("test correct result", function () {
-    assert.equal(computeGeometries( 12.571428571428571, 12.571428571428571, [12, 14], [20, 18], [0, 594]))
+    assert.equal(
+      computeGeometries(
+        12.571428571428571,
+        12.571428571428571,
+        [12, 14],
+        [20, 18],
+        [0, 594]
+      )
+    );
   });
 });
